8e74b06f1e089b9f8791e0c157381e5ce60091d2
Guilherme Cortez Lopes <guilhermecortezlopes20@gmail.com>
2026-09-09 23:30:12 -0300

    Tela Principal

--- parte2-flutter/lib/main.dart
+++ parte2-flutter/lib/main.dart
@@ -1,3 +1,30 @@
-// Ponto inicial do aplicativo Flutter.
-// A estrutura visual será implementada posteriormente.
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PlayRetroApp());
+}
+
+class PlayRetroApp extends StatelessWidget {
+  const PlayRetroApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Play Retrô',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF0D1117), // Fundo escuro
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF161B22),
+          centerTitle: true,
+        ),
+        colorScheme: ColorScheme.dark(
+          primary: Colors.blueAccent,
+          secondary: Colors.blueAccent.shade700,
+        ),
+      ),
+
+    );  
+  }
+}
